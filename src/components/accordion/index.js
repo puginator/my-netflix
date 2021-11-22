e8e52d9a44e2a6843a975f93fd3c934e4041d044
Austin Amento <puggsy31@aol.com>
2021-11-22 11:36:59 -0800

    styled accordion component

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Title, Body, Header, Container, Inner, Item, Frame} from './styles/accordion'
 
 export default function Accordion({children, ...restProps}) {
     return (
@@ -6,4 +7,24 @@ export default function Accordion({children, ...restProps}) {
             <Inner>{children}</Inner>
         </Container>
     )
+}
+
+Accordion.Frame = function AccordionFrame({children, ...restProps}) {
+    return <Frame {...restProps}>{children}</Frame>
+}
+
+Accordion.Item = function AccordionItem({children, ...restProps}) {
+    return <Item {...restProps}>{children}</Item>
+}
+
+Accordion.Title = function AccordionTitle({children, ...restProps}) {
+    return <Title {...restProps}>{children}</Title>
+}
+
+Accordion.Header = function AccordionHeader({children, ...restProps}) {
+    return <Header {...restProps}>{children}</Header>
+}
+
+Accordion.Body = function AccordionBody({children, ...restProps}) {
+    return <Body {...restProps}>{children}</Body>
 }
