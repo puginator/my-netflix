81396169b1d8775c2b0c80aa98e01e9bc9a5735d
Austin Amento <puggsy31@aol.com>
2021-11-26 16:58:06 -0800

    implemented live search

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -29,7 +29,18 @@ export function BrowseContainer({slides}) {
 
     useEffect(() => {
         setSlideRows(slides[category]);
-    }, [slides,category])
+    }, [slides,category]);
+
+    useEffect(() => {
+        const fuse = new Fuse(slideRows, {keys: ['data.description', 'data.title', 'data.genre']});
+        const results = fuse.search(searchTerm).map(({item}) => item);
+
+        if(slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+            setSlideRows(results);
+        } else {
+            setSlideRows(slides[category]);
+        }
+    }, [searchTerm]);
 
     return profile.displayName ? (
         <>
